6b543416e4c1388b785573a70148b857e7ef3b4a
xjh182 <xjh182067@gmail.com>
2020-05-14 21:01:42 +0800

    v0.1.5

--- 我方飞机.py
+++ 我方飞机.py
@@ -6,33 +6,44 @@ class 我方飞机(pygame.sprite.Sprite):
 
         self.图片1 = pygame.image.load("图片\玩家1.png").convert_alpha()
         self.图片2 = pygame.image.load("图片\玩家2.png").convert_alpha()
-        self.矩形 = self.图片1.get_rect()
+
+        self.摧毁的图片 = []
+        self.摧毁的图片.extend([\
+            pygame.image.load("图片\玩家炸1.png").convert_alpha(),
+            pygame.image.load("图片\玩家炸2.png").convert_alpha(),
+            pygame.image.load("图片\玩家炸3.png").convert_alpha(),
+            pygame.image.load("图片\玩家炸4.png").convert_alpha(),
+            ])
+
+        self.rect = self.图片1.get_rect()
         self.宽, self.高 = 背景尺寸[0], 背景尺寸[1]
-        self.矩形.left, self.矩形.top = \
-            (self.宽 - self.矩形.width) // 2,\
-                self.高 - self.矩形.height - 60
+        self.rect.left, self.rect.top = \
+            (self.宽 - self.rect.width) // 2,\
+                self.高 - self.rect.height - 60
         self.速度 = 10
+        self.活着 = True
+        self.mask = pygame.mask.from_surface(self.图片1)
 
     def 向上(self):
-        if self.矩形.top > 0:
-            self.矩形.top -= self.速度
+        if self.rect.top > 0:
+            self.rect.top -= self.速度
         else:
-            self.矩形.top = 0
+            self.rect.top = 0
 
     def 向下(self):
-        if self.矩形.bottom < self.高 - 60:
-            self.矩形.top += self.速度
+        if self.rect.bottom < self.高 - 60:
+            self.rect.top += self.速度
         else:
-            self.矩形.bottom = self.高 - 60
+            self.rect.bottom = self.高 - 60
 
     def 向左(self):
-        if self.矩形.left > 0:
-            self.矩形.left -= self.速度
+        if self.rect.left > 0:
+            self.rect.left -= self.速度
         else:
-            self.矩形.left = 0
+            self.rect.left = 0
 
     def 向右(self):
-        if self.矩形.right < self.宽:
-            self.矩形.left += self.速度
+        if self.rect.right < self.宽:
+            self.rect.left += self.速度
         else:
-            self.矩形.left = self.宽 - self.矩形.width
+            self.rect.left = self.宽 - self.rect.width
--- 敌机.py
+++ 敌机.py
@@ -6,22 +6,33 @@ class 小敌机(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         self.图片 = pygame.image.load("图片\敌人1.png").convert_alpha()
-        self.矩形 = self.图片.get_rect()
+
+        self.摧毁的图片 = []
+        self.摧毁的图片.extend([\
+            pygame.image.load("图片\敌人1炸1.png").convert_alpha(),
+            pygame.image.load("图片\敌人1炸2.png").convert_alpha(),
+            pygame.image.load("图片\敌人1炸3.png").convert_alpha(),
+            pygame.image.load("图片\敌人1炸4.png").convert_alpha()
+            ])
+        self.rect = self.图片.get_rect()
         self.宽, self.高 = 背景尺寸[0], 背景尺寸[1]
         self.速度 = 3
-        self.矩形.left, self.矩形.top = \
-            randint(0, self.宽 - self.矩形.width),\
+        self.活着 = True
+        self.mask = pygame.mask.from_surface(self.图片)
+        self.rect.left, self.rect.top = \
+            randint(0, self.宽 - self.rect.width),\
                 randint(-5 * self.高, 0)
 
     def 移动(self):
-        if self.矩形.top < self.高:
-            self.矩形.top += self.速度
+        if self.rect.top < self.高:
+            self.rect.top += self.速度
         else:
             self.重置
 
     def 重置(self):
-        self.矩形.left, self.矩形.top = \
-            randint(0, self.宽 - self.矩形.width),\
+        self.活着 = True
+        self.rect.left, self.rect.top = \
+            randint(0, self.宽 - self.rect.width),\
                 randint(-5 * self.高, 0)
 
 class 中敌机(pygame.sprite.Sprite):
@@ -29,23 +40,34 @@ class 中敌机(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         self.图片 = pygame.image.load("图片\敌人2.png").convert_alpha()
-        self.矩形 = self.图片.get_rect()
+
+        self.摧毁的图片 = []
+        self.摧毁的图片.extend([\
+            pygame.image.load("图片\敌人2炸1.png").convert_alpha(),
+            pygame.image.load("图片\敌人2炸2.png").convert_alpha(),
+            pygame.image.load("图片\敌人2炸3.png").convert_alpha(),
+            pygame.image.load("图片\敌人2炸4.png").convert_alpha()
+            ])
+        self.rect = self.图片.get_rect()
         self.宽, self.高 = 背景尺寸[0], 背景尺寸[1]
         self.速度 = 2
-        self.矩形.left, self.矩形.top = \
-            randint(0, self.宽 - self.矩形.width),\
+        self.活着 = True
+        self.mask = pygame.mask.from_surface(self.图片)
+        self.rect.left, self.rect.top = \
+            randint(0, self.宽 - self.rect.width),\
                 randint(-10 * self.高, -self.高)
 
     def 移动(self):
-        if self.矩形.top < self.高:
-            self.矩形.top += self.速度
+        if self.rect.top < self.高:
+            self.rect.top += self.速度
         else:
             self.重置
 
     def 重置(self):
-        self.矩形.left, self.矩形.top = \
-            randint(0, self.宽 - self.矩形.width),\
-                randint(-10 * self.高, -self.height)
+        self.活着 = True
+        self.rect.left, self.rect.top = \
+            randint(0, self.宽 - self.rect.width),\
+                randint(-10 * self.高, -self.高)
 
 class 大敌机(pygame.sprite.Sprite):
     def __init__(self, 背景尺寸):
@@ -53,20 +75,34 @@ class 大敌机(pygame.sprite.Sprite):
 
         self.图片1 = pygame.image.load("图片\敌人3_1.png").convert_alpha()
         self.图片2 = pygame.image.load("图片\敌人3_2.png").convert_alpha()
-        self.矩形 = self.图片1.get_rect()
+
+        self.摧毁的图片 = []
+        self.摧毁的图片.extend([\
+            pygame.image.load("图片\敌人3炸1.png").convert_alpha(),
+            pygame.image.load("图片\敌人3炸2.png").convert_alpha(),
+            pygame.image.load("图片\敌人3炸3.png").convert_alpha(),
+            pygame.image.load("图片\敌人3炸4.png").convert_alpha(),
+            pygame.image.load("图片\敌人3炸5.png").convert_alpha(),
+            pygame.image.load("图片\敌人3炸6.png").convert_alpha()
+            ])
+
+        self.rect = self.图片1.get_rect()
         self.宽, self.高 = 背景尺寸[0], 背景尺寸[1]
         self.速度 = 1
-        self.矩形.left, self.矩形.top = \
-            randint(0, self.宽 - self.矩形.width),\
+        self.活着 = True
+        self.mask = pygame.mask.from_surface(self.图片1)
+        self.rect.left, self.rect.top = \
+            randint(0, self.宽 - self.rect.width),\
                 randint(-15 * self.高, -5*self.高)
 
     def 移动(self):
-        if self.矩形.top < self.高:
-            self.矩形.top += self.速度
+        if self.rect.top < self.高:
+            self.rect.top += self.速度
         else:
             self.重置
 
     def 重置(self):
-        self.矩形.left, self.矩形.top = \
-            randint(0, self.宽 - self.矩形.width),\
+        self.活着 = True
+        self.rect.left, self.rect.top = \
+            randint(0, self.宽 - self.rect.width),\
                 randint(-15 * self.高, -5*self.height)
